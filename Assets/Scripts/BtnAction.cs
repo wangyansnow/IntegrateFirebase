2e5231b232f7fe1d4c41b44be8d004aec8f23dac
wangyan6@cmcm.com <wangyan6@cmcm.com>
2019-02-24 19:57:21 +0800

    initialize

---
+++ Assets/Scripts/BtnAction.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Firebase;
+using Firebase.Analytics;
+
+public class BtnAction : MonoBehaviour
+{
+
+    private void Awake()
+    {
+        //FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith()
+    }
+
+    public void ReportBtnClick()
+    {
+        Debug.Log("Report Btn click");
+
+
+    }
+}
